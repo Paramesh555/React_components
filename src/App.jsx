15edf33cb7d2b0a8e587ee529e452d8b210ae51f
Paramesh555 <103727256+Paramesh555@users.noreply.github.com>
2022-10-03 10:56:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import MyList from "./List";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <MyList />
+    </div>
+  );
+}
+export default App;
---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MyList() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default MyList;
